3583c8d2b6d05d19d02e446fbf2366fa6cb58e1e
Magdalena <magdalena.skawinska@gmail.com>
2023-08-24 14:28:18 +0200

    Initial Commit

---
+++ SwiftUIWithUIKitComponents/SwiftUIWithUIKitComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWithUIKitComponentsApp.swift
+//  SwiftUIWithUIKitComponents
+//
+//  Created by magdalena.skawinska on 24/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWithUIKitComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
